1f34e9ded4ff700358c8920dc6473c499aafc3e3
omerfbasaran <omerfbasaran@gmail.com>
2023-07-13 22:11:22 +0300

    ArtBook fetching data from coreData

--- ArtBook/ArtBook/DetailsVC.swift
+++ ArtBook/ArtBook/DetailsVC.swift
@@ -6,8 +6,9 @@
 //
 
 import UIKit
+import CoreData
 
-class DetailsVC: UIViewController {
+class DetailsVC: UIViewController , UIImagePickerControllerDelegate , UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     
@@ -21,19 +22,57 @@ class DetailsVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.isUserInteractionEnabled = true
         
+        view.isUserInteractionEnabled = true
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(closeKeyboard))
         view.addGestureRecognizer(gestureRecognizer)
-        // Do any additional setup after loading the view.
+        
+        imageView.isUserInteractionEnabled = true
+        let imageTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(selectImage))
+        imageView.addGestureRecognizer(imageTapGestureRecognizer)
+       
     }
     
     @objc func closeKeyboard(){
         view.endEditing(true)
         print("test")
     }
+    
+    @objc func selectImage(){
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        present(picker, animated: true)
+    }
 
     @IBAction func saveButtonClicked(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let newPainting = NSEntityDescription.insertNewObject(forEntityName: "Paintings", into: context)
+        
+        newPainting.setValue(nameTextField.text ?? "", forKey: "name")
+        newPainting.setValue(artistTextField.text ?? "", forKey: "artist")
+        
+        if let year = Int(yearTextField.text!) {
+            newPainting.setValue(year, forKey: "year")
+        }
+        
+        newPainting.setValue(UUID(), forKey: "id")
+        
+        let imageData = imageView.image?.jpegData(compressionQuality: 0.5)
+        newPainting.setValue(imageData, forKey: "image")
+        
+        do {
+            try context.save()
+            print("saved Succes")
+        }   catch {
+            print("error when saved")
+        }
+        
     }
-    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true)
+    }
 }
--- ArtBook/ArtBook/ViewController.swift
+++ ArtBook/ArtBook/ViewController.swift
@@ -6,20 +6,65 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    
 
     @IBOutlet weak var tableView: UITableView!
     
+    var nameArray = [String]()
+    var idArray = [UUID]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(plusClicked))
         // Do any additional setup after loading the view.
+        getData()
+    }
+    
+    func getData(){
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do {
+            let results = try context.fetch(fetchRequest)
+            
+            for result in results as! [NSManagedObject] {
+                if let name = result.value(forKey: "name") as? String{
+                    self.nameArray.append(name)
+                }
+                if let id = result.value(forKey: "id") as? UUID {
+                    self.idArray.append(id)
+                }
+            }
+        }catch{
+            print("error when fetching")
+        }
+        self.tableView.reloadData()
+
+        
     }
     @objc func plusClicked() {
         performSegue(withIdentifier: "toDetailsVC", sender: nil)
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nameArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = nameArray[indexPath.row]
+        return cell
+    }
 }
 
